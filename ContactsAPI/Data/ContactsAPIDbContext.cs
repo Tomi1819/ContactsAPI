a1a0583cdac8a82a7c1ce97f16a4c228e74e6f0f
Tomi1819 <toshevtomi@gmail.com>
2022-11-27 16:21:35 +0200

    create get all contacts method

--- ContactsAPI/Data/ContactsAPIDbContext.cs
+++ ContactsAPI/Data/ContactsAPIDbContext.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ContactsAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContactsAPI.Data
 {
-    public class ContactsAPIDbContext: DbContext
+    public class ContactsAPIDbContext : DbContext
     {
-
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; }
     }
 }
